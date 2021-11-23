8bbc06a6ab68d1eda69a8ea799a30c1350d01ff7
darnordd <paperp.devteam@gmail.com>
2021-11-23 19:02:40 +0700

    Commit message

--- main.py
+++ main.py
@@ -13,24 +13,27 @@ class Example(QWidget):
     def initUI(self):
         self.setGeometry(300, 300, 500, 500)
         self.setWindowTitle('Желтые окружности')
-
+        self.do_paint = False
         self.btn_spawn = QPushButton('Press to spawn circle', self)
         self.btn_spawn.resize(125, 125)
         self.btn_spawn.move(200, 350)
         self.btn_spawn.clicked.connect(self.spawn)
 
     def spawn(self):
+        self.do_paint = True
         self.repaint()
 
     def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
-        x = random.randint(10, 100)
-        xx = random.randint(x + 1, 499 - x)
-        yy = random.randint(x + 1, 299 - x)
-        qp.drawEllipse(QPoint(xx, yy), x, x)
-        qp.end()
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor(255, 255, 0))
+            x = random.randint(10, 100)
+            xx = random.randint(x + 1, 499 - x)
+            yy = random.randint(x + 1, 299 - x)
+            qp.drawEllipse(QPoint(xx, yy), x, x)
+            qp.end()
+            self.do_paint = False
 
 
 if __name__ == '__main__':
