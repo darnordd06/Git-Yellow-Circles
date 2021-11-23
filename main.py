7f2ad9d1f9cb5339e33ab291904ce4140435d6f7
darnordd <paperp.devteam@gmail.com>
2021-11-23 18:26:16 +0700

    Commit message

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
-
+import random
+from PyQt5.QtCore import QPoint, Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtWidgets import QLabel, QLineEdit
+from PyQt5.QtGui import QPainter, QColor
 
 
 class Example(QWidget):
@@ -10,5 +11,30 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 400, 150)
-        self.setWindowTitle('Арифмометр')
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('Желтые окружности')
+
+        self.btn_spawn = QPushButton('Press to spawn circle', self)
+        self.btn_spawn.resize(125, 125)
+        self.btn_spawn.move(200, 350)
+        self.btn_spawn.clicked.connect(self.spawn)
+
+    def spawn(self):
+        self.repaint()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QColor(255, 255, 0))
+        x = random.randint(10, 100)
+        xx = random.randint(x + 1, 499 - x)
+        yy = random.randint(x + 1, 299 - x)
+        qp.drawEllipse(QPoint(xx, yy), x, x)
+        qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
